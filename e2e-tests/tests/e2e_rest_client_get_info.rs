5ebe6a2a9997529eefa63bdc0c4953d244875416
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2025-09-08 14:47:25 +1000

    fix(e2e): Retry if e2e_rest_client_get_info fails
    Our e2e tests tend to start earlier than arkd is actually ready.

--- e2e-tests/tests/e2e_rest_client_get_info.rs
+++ e2e-tests/tests/e2e_rest_client_get_info.rs
@@ -1,8 +1,30 @@
 use ark_rest::Client;
+use std::time::Duration;
 
 #[tokio::test]
 #[ignore]
 async fn can_get_info_from_ark_server() {
     let client = Client::new("http://localhost:7070".to_string());
-    let _info = client.get_info().await.unwrap();
+
+    let mut n_retries = 0;
+    while n_retries < 5 {
+        let res = client.get_info().await;
+
+        match res {
+            Ok(_) => {
+                return;
+            }
+            Err(error) => {
+                tracing::warn!(?error, "Failed to get info, retrying");
+
+                tokio::time::sleep(Duration::from_secs(2)).await;
+
+                n_retries += 1;
+
+                continue;
+            }
+        };
+    }
+
+    panic!("Failed to get info after several retries");
 }
