0e14906e06aa03741ae0119e3fdbd9418d7b6be8
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2026-04-17 09:31:18 +1000

    test(e2e): add fulmine delegator smoke test with watcher

--- e2e-tests/tests/common.rs
+++ e2e-tests/tests/common.rs
@@ -420,6 +420,50 @@ pub async fn set_up_client(
     (client, wallet)
 }
 
+#[allow(unused)]
+pub async fn set_up_client_with_delegator(
+    name: String,
+    nigiri: Arc<Nigiri>,
+    secp: Secp256k1<All>,
+    delegator_pk: XOnlyPublicKey,
+) -> (
+    Client<Nigiri, Wallet<InMemoryDb>, InMemorySwapStorage, Bip32KeyProvider>,
+    Arc<Wallet<InMemoryDb>>,
+) {
+    let mut rng = thread_rng();
+
+    let sk = SecretKey::new(&mut rng);
+    let kp = Keypair::from_secret_key(&secp, &sk);
+
+    let network = Network::Regtest;
+
+    let db = InMemoryDb::default();
+    let wallet = Wallet::new(kp, secp, network, "http://localhost:3000", db).unwrap();
+    let wallet = Arc::new(wallet);
+
+    let seed: [u8; 32] = rng.r#gen();
+    let xpriv = Xpriv::new_master(network, &seed).unwrap();
+
+    let client = OfflineClient::<_, _, _, Bip32KeyProvider>::new_with_bip32(
+        name,
+        xpriv,
+        None,
+        nigiri,
+        wallet.clone(),
+        "http://localhost:7070".to_string(),
+        Arc::new(InMemorySwapStorage::default()),
+        "http://localhost:9001".to_string(),
+        Duration::from_secs(30),
+        Some(delegator_pk),
+        vec![delegator_pk],
+    )
+    .connect_with_retries(5)
+    .await
+    .unwrap();
+
+    (client, wallet)
+}
+
 /// Wait until the client's offchain balance matches the specified targets.
 ///
 /// Usage:
