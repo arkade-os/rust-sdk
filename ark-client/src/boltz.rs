9d52433e8229a28b3537a64f8a92c158e71ed83b
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2026-07-15 17:27:09 +1000

    chore: Appease Clippy 1.97.0

--- ark-client/src/boltz.rs
+++ ark-client/src/boltz.rs
@@ -2744,7 +2744,7 @@ where
             .map_err(|e| Error::ad_hoc(e.to_string()))?;
 
         // Fetch submarine swap fees (Arkade -> BTC)
-        let submarine_url = format!("{}/v2/swap/submarine", &self.inner.boltz_url);
+        let submarine_url = format!("{}/v2/swap/submarine", self.inner.boltz_url);
         let submarine_response = client
             .get(&submarine_url)
             .send()
